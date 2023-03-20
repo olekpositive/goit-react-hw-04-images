d94d24f3ee5bec9ad73a14696f473e74dfa34657
Oleksandr Kihim <oleksandr.kigim@gmail.com>
2023-03-20 17:04:10 +0100

    Update_2.0_hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
@@ -8,67 +8,59 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 
-export class App extends React.Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    totalHits: 0,
-    isLoading: false,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  async componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-
-    if (prevState.query !== query || prevState.page !== page) {
-      try {
-        this.setState({ isLoading: true });
-
-        const { totalHits, hits } = await fetchImages(query, page);
-
-        if (totalHits === 0) {
-          toast.error('Nothing was found for your request');
-          this.setState({ isLoading: false });
-          return;
-        }
-
-        this.setState(prevState => ({
-          images: page === 1 ? hits : [...prevState.images, ...hits],
-
-          totalHits:
-            page === 1
-              ? totalHits - hits.length
-              : totalHits - [...prevState.images, ...hits].length,
-        }));
-
-        this.setState({ isLoading: false });
-      } catch (error) {
-        toast.error(`Oops! Something went wrong! ${error}`);
-      }
+  useEffect(() => {
+    if (!query) {
+      return;
     }
-  }
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+    setLoading(true);
+
+    const fetchData = async () => {
+      const { hits, totalHits } = await fetchImages(query, page);
+
+      if (totalHits === 0) {
+        toast.error('Nothing was found for your request');
+        setLoading(false);
+        return;
+      }
+
+      setImages(prevImages => (page === 1 ? hits : [...prevImages, ...hits]));
+      setTotalHits(prevTotalHits =>
+        page === 1 ? totalHits - hits.length : prevTotalHits - hits.length
+      );
+      setLoading(false);
+    };
+
+    fetchData().catch(error => {
+      toast.error(`Oops! Something went wrong! ${error}`);
+      setLoading(false);
+    });
+  }, [page, query]);
+
+  const handleQuerySubmit = query => {
+    setQuery(query);
+    setPage(1);
   };
 
-  handleQuerySubmit = query => {
-    this.setState({ query, page: 1 });
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { images, totalHits, isLoading } = this.state;
-    const { handleQuerySubmit, handleLoadMore } = this;
+  return (
+    <>
+      <Searchbar onSubmit={handleQuerySubmit} />
+      {images && <ImageGallery images={images} />}
+      {!!totalHits && <Button onLoadMore={handleLoadMore} />}
+      {loading && <Loader />}
 
-    return (
-      <>
-        <Searchbar onSubmit={handleQuerySubmit} />
-        {images && <ImageGallery images={images} />}
-        {!!totalHits && <Button onLoadMore={handleLoadMore} />}
-        {isLoading && <Loader />}
-
-        <ToastContainer autoClose={2000} />
-      </>
-    );
-  }
-}
+      <ToastContainer autoClose={2000} />
+    </>
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,55 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import { Header, SearchForm, Input, ButtonSubmit } from './Searchbar.styled';
 
-export class Searchbar extends React.Component {
-  state = {
-    query: '',
+export const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearchQueryChange = e => {
+    setQuery(e.currentTarget.value.toLowerCase());
   };
 
-  handleSearchQueryChange = e => {
-    this.setState({ query: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    const { query } = this.state;
-    const { onSubmit } = this.props;
 
     if (query.trim() === '') {
       toast.error('Please enter a search value');
       return;
     }
-
     onSubmit(query);
   };
 
-  render() {
-    const { handleSubmit, handleSearchQueryChange } = this;
-    const { query } = this.state;
+  return (
+    <Header className="searchbar">
+      <SearchForm onSubmit={handleSubmit} className="form">
+        <Input
+          onChange={handleSearchQueryChange}
+          value={query}
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
 
-    return (
-      <Header className="searchbar">
-        <SearchForm onSubmit={handleSubmit} className="form">
-          <Input
-            onChange={handleSearchQueryChange}
-            value={query}
-            className="input"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-
-          <ButtonSubmit type="submit" className="button">
-            Search
-          </ButtonSubmit>
-        </SearchForm>
-      </Header>
-    );
-  }
-}
+        <ButtonSubmit type="submit" className="button">
+          Search
+        </ButtonSubmit>
+      </SearchForm>
+    </Header>
+  );
+};
 
 Searchbar.propTypes = { onSubmit: PropTypes.func.isRequired };
